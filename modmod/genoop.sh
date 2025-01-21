48517779ed37b03b04ebaa0360952b17d791f005
Tobias Schmitt-Lechner <tobias.schmitt-lechner@andrena.de>
2025-01-21 12:13:57 +0100

    feat: pdf generation

--- modmod/genoop.sh
+++ modmod/genoop.sh
@@ -1,2 +1,23 @@
-cargo run -- generate -o target/oopcourse -c ../content/full.track.toml --theme @mandel/slidev-theme-sandbee
+#cargo run -- generate -o target/oopcourse -c ../content/full.track.toml --theme @mandel/slidev-theme-sandbee
+cargo run -- generate -o target/oopcourse ../content/full.track.toml --theme @mandel/slidev-theme-sandbee
 cp ../oop/layouts target/oopcourse/slides -r
+cp ../oop/.npmrc target/oopcourse/slides
+
+pushd target/oopcourse/slides
+npm add @mandel/slidev-theme-sandbee@1.5.3
+npm install
+sed -i 's/slidev export/slidev export --with-clicks/g' package.json
+npm run export-1_1
+npm run export-2_1
+npm run export-2_2
+npm run export-2_3
+npm run export-2_4
+npm run export-2_5
+npm run export-2_6
+npm run export-3_1
+npm run export-4_1
+npm run export-4_2
+npm run export-4_3
+npm run export-5_1
+npm run export-6_1
+popd
